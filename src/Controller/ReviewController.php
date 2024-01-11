69b2087fd3c968c4142055a07c644e9779ea36fb
ALIXZAMORO <alix.zamoro7@gmail.com>
2024-01-11 20:32:06 +0100

    formulaire MAJ ouf

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Review;
+use App\Form\ReviewType;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,15 +15,26 @@ class ReviewController extends AbstractController
     /**
      * @Route("/movies/{id}/review/add", name="app_review_add", requirements={"id": "\d+"})
      */
-    public function index($id, MovieRepository $movieRepository): Response
+    public function index($id, MovieRepository $movieRepository, Request $request): Response
     {
         $movie = $movieRepository->find($id);
 
         if ($movie === null) { throw $this->createNotFoundException("ce film n'existe pas");}
 
-        return $this->render('review/index.html.twig', [
+        $newReview = new Review();
+        $form = $this->createForm(ReviewType::class, $newReview);
 
-            "movieFromBDD" => $movie
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted())
+        {
+            dd($newReview);
+        }
+
+        return $this->renderForm('review/index.html.twig', [
+
+            "movieFromBDD" => $movie,
+            "formulaire" => $form
             
         ]);
     }
