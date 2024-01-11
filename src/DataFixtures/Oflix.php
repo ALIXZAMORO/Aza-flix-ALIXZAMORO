e9f360917287d361043c524a9317e7139c415768
ALIXZAMORO <alix.zamoro7@gmail.com>
2024-01-11 15:30:52 +0100

    fixtures type & genre

--- src/DataFixtures/Oflix.php
+++ src/DataFixtures/Oflix.php
@@ -2,15 +2,46 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Genre;
+use App\Entity\Type;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class Oflix extends Fixture
 {
+    /**
+     * Création de donnée
+     *
+     * @param ObjectManager $manager
+     * @return void
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $genres = [
+            'horreur',
+            'comique',
+            'sf'
+        ];
+
+        for ($i=1; $i <= 10; $i++) { 
+            
+            $newGenre = new Genre();
+
+            $newGenre->setName("Genre #" . $i);
+
+            $manager->persist($newGenre);
+        }
+
+        $types = ["film", "série"];
+
+        foreach ($types as $type) {
+            
+            $newType = new Type();
+
+            $newType->setName($type);
+
+            $manager->persist($newType);
+        }
 
         $manager->flush();
     }
