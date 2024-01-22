2a0e7ef82108ac7fdb0d8f36787572595b531af8
ALIXZAMORO <alix.zamoro7@gmail.com>
2024-01-22 18:17:30 +0100

    tri par titre season

--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Movie;
 use App\Entity\Season;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,7 +21,19 @@ class SeasonType extends AbstractType
                 "multiple" => false,
                 "expanded" => false,
                 "class" => Movie::class,
-                "choice_label" => "title"
+                "choice_label" => "title",
+                "query_builder" => function(EntityRepository $entityrepository){
+                    // TODO : requete perso : tri par titre
+                    // TODO : requete perso : que le type série
+                    return $entityrepository->createQueryBuilder('m')
+                        // ! Error: Class App\Entity\Movie has no field or association named type.name    
+                        // ->where("m.type.name = 'série'")
+                        // * on fait une jointure sur l'objet Type
+                        ->join("m.type", "t")
+                        // on fait le where sur les propriété de l'objet Type
+                        ->where("t.name = 'série'")
+                        ->orderBy('m.title', 'ASC');
+                }
             ])
         ;
     }
