077a5e1aa551826dea590cc280b8326d7def9d2f
ALIXZAMORO <alix.zamoro7@gmail.com>
2024-01-24 15:27:25 +0100

    useredittype ok

---
+++ src/Form/UserEditType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('email', EmailType::class, [
+            "label" => "Email pour se logger"
+        ])
+            
+            ->add('password',PasswordType::class, [
+                "label" => "le mot de passe"
+            ])
+
+            ->add('roles', ChoiceType::class, [
+                "multiple" => true,
+                "expanded" => true,
+                "choices" => [
+                    "ADMIN" => "ROLE_ADMIN",
+                    "MANAGER" => "ROLE_MANAGER",
+                    "USER" => "ROLE_USER"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            "attr" => ["novalidate" => "novalidate"]
+        ]);
+    }
+}
