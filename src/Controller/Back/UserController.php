64fbd9d907fec13439f4becced4b43c82625ea26
ALIXZAMORO <alix.zamoro7@gmail.com>
2024-01-24 15:11:10 +0100

    user hashed ok

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -8,6 +8,8 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -59,12 +61,19 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="app_back_user_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(Request $request, User $user, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $plainPassword = $user->getPassword();
+
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
+            
+            $user->setPassword($hashedPassword);
+
             $userRepository->add($user, true);
 
             return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,23 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+        ->add('email', EmailType::class, [
+            "label" => "Email pour se logger"
+        ])
+            
+            ->add('password',PasswordType::class, [
+                "label" => "le mot de passe"
+            ])
+
+            ->add('roles', ChoiceType::class, [
+                "multiple" => true,
+                "expanded" => true,
+                "choices" => [
+                    "ADMIN" => "ROLE_ADMIN",
+                    "MANAGER" => "ROLE_MANAGER",
+                    "USER" => "ROLE_USER"
+                ]
+            ])
         ;
     }
 
@@ -22,6 +39,7 @@ class UserType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            "attr" => ["novalidate" => "novalidate"]
         ]);
     }
 }
