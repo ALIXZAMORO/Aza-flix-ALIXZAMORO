9c0a0ad3e731b1efea31d20c0ef7396f1f940269
ALIXZAMORO <alix.zamoro7@gmail.com>
2024-01-25 17:53:56 +0100

    maintenance ok

---
+++ src/EventSubscriber/MaintenanceSubscriber.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class MaintenanceSubscriber implements EventSubscriberInterface
+{
+
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+
+        //dd($event);
+        // on trouve dans l'argument $event :
+
+        // * request, avec pathInfo
+        // va nous servir pour tester la route et exclure certaine route
+        $pathInfo = $event->getRequest()->getPathInfo();
+        // dd($pathInfo); // /back/main
+        // ? https://www.php.net/manual/fr/function.strpos.php
+        // si PHP 8 : https://www.php.net/manual/en/function.str-starts-with.php
+        // cette fonction PHP nous sert à savoir si la chaine de caractère commence par ...
+        if (strpos($pathInfo, "/back") === 0){
+            // on est sur une route du back, on s'arrête là
+            return;
+        }
+
+        // * response, avec le content
+        $response = $event->getResponse();
+        $content = $response->getContent();
+        // dd($content);
+        // on cherche l'élement HTML où se placer : <div class="container">
+        // et on remplace cet élément par notre message
+        $modifiedContent = str_replace(
+            // notre recherche
+            '<div class="container">', 
+            // on remplace par ...
+            '<div class="container">
+                <div class="alert alert-danger">Maintenance prévue jeudi 25 mai à 17h00</div>
+            ',
+            // sur quel contenu
+            $content
+        );
+        // il reste à mettre à jour le contenu de la response
+        $response->setContent($modifiedContent);
+        
+        // version en une seule instruction
+        /*
+        $event->getResponse()->setContent(str_replace(
+            // notre recherche
+            '<div class="container">', 
+            // on remplace par ...
+            '<div class="container">
+                <div class="alert alert-danger">Maintenance prévue jeudi 25 mai à 17h00</div>
+            ',
+            // sur quel contenu
+            $response->getContent()
+        ));
+        */
+
+        // comme la méthode renvoit void, pas de return à faire de notre coté.
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // en clé : le nom de l'évènement
+            // en valeur : le nom de la méthode à éxécuter
+            'kernel.response' => 'onKernelResponse',
+        ];
+    }
+}
---
+++ src/Services/OmdbApi.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Services;
+
+// composer require symfony/http-client
+
+use App\Models\OmdbApiModel;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class OmdbApi 
+{
+    /**
+     * Clé pour accéder à l'API
+     *
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * Service client HTTP
+     *
+     * @var HttpClientInterface
+     */
+    private $client;
+
+    /**
+     * Service de désérialisation
+     *
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(HttpClientInterface $client,SerializerInterface $serializerInterface, $apiKey)
+    {
+        $this->client = $client;
+        $this->apiKey = $apiKey;
+        $this->serializer = $serializerInterface;
+    }
+
+    public function fetch(string $title): OmdbApiModel
+    {
+        $response = $this->client->request(
+            'GET',
+            'http://www.omdbapi.com/', [
+                // these values are automatically encoded before including them in the URL
+                'query' => [
+                    't' => $title,
+                    'apiKey' => $this->apiKey,
+                ],
+            ]
+        );
+
+        // $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        // $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        // $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+        
+        /** @var OmdbApiModel $omdbApiModel */
+        $omdbApiModel = $this->serializer->deserialize($content, OmdbApiModel::class, 'json');
+
+        // dd($omdbApiModel);
+
+        return $omdbApiModel;
+    }
+}
+
+
