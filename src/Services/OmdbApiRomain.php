7823633d7c9b1c5230049ca4b2cf44892ff4ebcd
ALIXZAMORO <alix.zamoro7@gmail.com>
2024-01-25 18:07:14 +0100

    ombdapi ok

---
+++ src/Services/OmdbApiRomain.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+// composer require symfony/http-client
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class OmdbApiRomain 
+{
+    // Clé  dans .env
+    private $apiKey;
+    /**
+     * Service client HTTP
+     *
+     * @var HttpClientInterface
+     */
+    private $client;
+
+
+    public function __construct(HttpClientInterface $client, $apiKey)
+    {
+        $this->client = $client;
+        $this->apiKey = $apiKey;
+    }
+
+
+    public function fetch(string $title): array
+    {
+        $response = $this->client->request(
+            'GET',
+            'http://www.omdbapi.com/', [
+                // these values are automatically encoded before including them in the URL
+                'query' => [
+                    't' => $title,
+                    'apiKey' => $this->apiKey,
+                ],
+            ]
+        );
+
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+        
+        return $content;
+    }
+}
